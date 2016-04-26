65b411b020dadd9b9cd771c6adc6a6ffe47d24d7
Conor Schaefer <conor@freedom.press>
2016-04-26 11:37:16 -0700

    Creates ServerSpec tests for OpenSSL files
    Checks key length, rough validity in days, and the autogenerated
    subject name for the certificate file.
    
    For the private key, ensures it's a key, rather than a cert, and
    confirms it matches the automatically generated certificate file,
    as well.

--- spec/generate_ssl_cert_spec.rb
+++ spec/generate_ssl_cert_spec.rb
@@ -19,3 +19,25 @@ describe file('/etc/pki/tls/private') do
   its('group') { should eq 'root' }
   its('mode') { should eq '700' }
 end
+
+cert_path = '/etc/pki/tls/certs/logstash-client.crt'
+key_path = '/etc/pki/tls/private/logstash-client.key'
+
+describe x509_certificate(cert_path) do
+  it { should be_certificate }
+  it { should be_valid }
+  its(:subject) { should eq '/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd' }
+  its(:keylength) { should be >= 2048 }
+  its(:validity_in_days) { should_not be < 100 }
+  its(:validity_in_days) { should be >= 100 }
+  # Ostensibly a superfluous test, but if using custom values, only one test
+  # will fail, and the others (e.g. '>= 100') will still pass, improving the
+  # quality of the error output.
+  its(:validity_in_days) { should be >= 3000 }
+end
+
+describe x509_private_key(key_path) do
+  it { should be_valid }
+  it { should_not be_encrypted }
+  it { should have_matching_certificate(cert_path) }
+end
